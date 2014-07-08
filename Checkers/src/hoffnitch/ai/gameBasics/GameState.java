92d73c11f4b5b9397ee9cb5857ebf99683d43833
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2014-07-08 19:42:49 -0400

    Add basic structure to GameState

--- Checkers/src/hoffnitch/ai/gameBasics/GameState.java
+++ Checkers/src/hoffnitch/ai/gameBasics/GameState.java
@@ -1,5 +1,29 @@
 package hoffnitch.ai.gameBasics;
 
+
 public class GameState {
 
+	public static final int WIDTH = 8;
+	
+	private Piece[][] boardPieces;
+	
+	public GameState() {
+		boardPieces = new Piece[WIDTH][WIDTH];
+	}
+	
+	public Piece getPiece(Position position) {
+		return boardPieces[position.getX()][position.getY()];
+	}
+	
+	public void setPiece(Piece piece, Position position) {
+		boardPieces[position.getX()][position.getY()] = piece;
+	}
+	
+	/**
+	 * TODO: implement GameState.getState()
+	 * @return current game state
+	 */
+	public Piece[][] getState() {
+		return null;
+	}
 }
