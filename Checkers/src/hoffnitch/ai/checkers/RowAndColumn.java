49f53902f238847412354f6070bc9642545dd6d3
Mike <mikenitchie@gmail.com>
2014-09-05 16:57:20 -0400

    Adding 'RowAndColumn' class to encapsulate coordinates into one object.

---
+++ Checkers/src/hoffnitch/ai/checkers/RowAndColumn.java
@@ -0,0 +1,13 @@
+package hoffnitch.ai.checkers;
+
+public class RowAndColumn {
+    public final int row;
+    public final int column;
+    
+    public RowAndColumn(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    
+    //TODO: Implement equals and hashcode.
+}
