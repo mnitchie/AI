be296b19598c50e84237619c32c5a5153db20afd
Mike <mikenitchie@gmail.com>
2014-10-02 20:46:56 -0400

    Implemented 'evaluateBoard' for RandomBot

--- Checkers/src/hoffnitch/ai/checkers/ai/RandomBot.java
+++ Checkers/src/hoffnitch/ai/checkers/ai/RandomBot.java
@@ -5,10 +5,12 @@ import hoffnitch.ai.checkers.PieceColor;
 import hoffnitch.ai.checkers.Turn;
 
 import java.util.List;
+import java.util.Random;
 
 public class RandomBot extends AIPlayer {
     
     private static final String HEURISTIC_DESCRIPTION = "Random Bot";
+    public static final Random generator = new Random();
 
 	public RandomBot(PieceColor color) {
 		super(HEURISTIC_DESCRIPTION, color);
@@ -22,7 +24,9 @@ public class RandomBot extends AIPlayer {
 	
 	@Override
 	public double evaluateBoard(GameState board) {
-	    return 0;
+	    boolean negative = generator.nextInt(2) == 1 ? true : false;
+	    double num = generator.nextDouble();
+	    return negative ? -num : num;
 	}
 	
 	@Override
