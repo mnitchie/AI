3b0c9947a4e77508b7cc8ec19bd2825eb9091280
Mike <mikenitchie@gmail.com>
2014-10-02 20:42:14 -0400

    Killed the 'EagerBot' as it doesn't make sense with our new evaluation strategy.

--- Checkers/src/hoffnitch/ai/checkers/ai/PlayerFactory.java
+++ Checkers/src/hoffnitch/ai/checkers/ai/PlayerFactory.java
@@ -1,22 +1,17 @@
 package hoffnitch.ai.checkers.ai;
 
 import hoffnitch.ai.checkers.HumanPlayer;
-import hoffnitch.ai.checkers.PieceColor;
 import hoffnitch.ai.checkers.Player;
 import hoffnitch.ai.checkers.gui.BoardCanvas;
 import hoffnitch.ai.checkers.gui.newGameMenu.PlayerInfo;
 
 import java.util.ArrayList;
 
-import checkersRemote.CheckersConnector;
-import checkersRemote.RemotePlayerInfo;
-
 public class PlayerFactory {
 
 	public static final String HUMAN 			= "Human Player";
 	public static final String REMOTE_PLAYER	= "Remote Player";
 	public static final String RANDOM_BOT 		= "RandomBot";
-	public static final String EAGER_BOT 		= "EagerBot";
 	
 	private ArrayList<String> playerTypes;
 	
@@ -25,7 +20,6 @@ public class PlayerFactory {
 
 		playerTypes.add(HUMAN);
 		playerTypes.add(RANDOM_BOT);
-		playerTypes.add(EAGER_BOT);
 	}
 	
 	public Player getPlayer(PlayerInfo playerInfo, BoardCanvas view) {
@@ -34,8 +28,6 @@ public class PlayerFactory {
 			return new HumanPlayer(playerInfo.getPlayerName(), playerInfo.getColor(), view);
 		case RANDOM_BOT:
 			return new RandomBot(playerInfo.getColor());
-		case EAGER_BOT:
-			return new EagerBot(playerInfo.getColor());
 		}
 		return null;
 	}
