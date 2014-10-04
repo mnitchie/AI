8e1b7afac68fb9dcb590b84d773b8e87aca7ed57
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2014-10-04 13:28:55 -0400

    Add CheckersTree field to AIPlayer

--- Checkers/src/hoffnitch/ai/checkers/ai/AIPlayer.java
+++ Checkers/src/hoffnitch/ai/checkers/ai/AIPlayer.java
@@ -4,25 +4,36 @@ import hoffnitch.ai.checkers.GameState;
 import hoffnitch.ai.checkers.PieceColor;
 import hoffnitch.ai.checkers.Player;
 import hoffnitch.ai.checkers.Turn;
+import hoffnitch.ai.checkers.exceptions.InvalidTurnException;
+import hoffnitch.ai.search.CheckersTree;
 
 import java.util.List;
 
 public abstract class AIPlayer extends Player {
 	
+	private CheckersTree turnTree;
+	
 	public AIPlayer(String name, PieceColor color) {
 		super(name, color);
 	}
 	
+	public void setBoard(GameState initialBoard, PieceColor firstColor, int maxDepth) {
+		turnTree = new CheckersTree(initialBoard, firstColor, maxDepth);
+	}
+	
+	public void getOpponentTurn(Turn turn) throws InvalidTurnException {
+		turnTree.doOpponentTurn(turn);
+		turnTree.evaluateNodes(this);
+	}
+	
+	
 	/**
-	 * TODO: make abstract
-	 * 
 	 * @param board
-	 * @return
+	 * @return Returns double representing board evaluation (higher values are better)
 	 */
 	public abstract double evaluateBoard(GameState board);
 	
 	/**
-	 * 
 	 * @param options
 	 * @return
 	 */
