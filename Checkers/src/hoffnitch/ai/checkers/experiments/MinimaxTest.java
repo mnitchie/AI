46ba686896a1a8617df49721dab0f0fce74e2f75
Mike <mikenitchie@gmail.com>
2014-10-04 13:12:02 -0400

    Minimax test

--- Checkers/src/hoffnitch/ai/checkers/experiments/MinimaxTest.java
+++ Checkers/src/hoffnitch/ai/checkers/experiments/MinimaxTest.java
@@ -1,7 +1,25 @@
 package hoffnitch.ai.checkers.experiments;
 
+import hoffnitch.ai.checkers.GameState;
+import hoffnitch.ai.checkers.PieceColor;
+import hoffnitch.ai.checkers.Turn;
+import hoffnitch.ai.checkers.ai.AIPlayer;
+import hoffnitch.ai.checkers.ai.RandomBot;
+import hoffnitch.ai.search.CheckersTree;
+
 public class MinimaxTest {
     public static void main(String[] args) {
+        GameState board = new GameState();
+        AIPlayer randomDark = new RandomBot(PieceColor.DARK);
         
+        CheckersTree tree = new CheckersTree(board, PieceColor.DARK, 6);
+        System.out.println("Wait here...");
+        
+        tree.evaluateNodes(randomDark);
+        System.out.println("And here...");
+        
+        Turn t = tree.getBestTurn();
+        System.out.println(t);
+
     }
 }
