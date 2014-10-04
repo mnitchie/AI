5d362e7f52ae3da05d7b5503f43e7f10c6305ef5
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2014-10-04 13:08:34 -0400

    Add RatioBot

---
+++ Checkers/src/hoffnitch/ai/checkers/ai/RatioBot.java
@@ -0,0 +1,35 @@
+package hoffnitch.ai.checkers.ai;
+
+import hoffnitch.ai.checkers.GameState;
+import hoffnitch.ai.checkers.PieceColor;
+import hoffnitch.ai.checkers.Turn;
+
+import java.util.List;
+
+public class RatioBot extends AIPlayer
+{
+	public static final String HEURISTIC_DESCRIPTION = "Count Bot";
+	
+	public RatioBot(PieceColor color) {
+		super(HEURISTIC_DESCRIPTION, color);
+	}
+	
+	@Override
+	public Turn getTurn(List<Turn> options) {
+		int random = (int)Math.floor(Math.random() * options.size());
+		return options.get(random);
+	}
+	
+	@Override
+	public double evaluateBoard(GameState board) {
+	    double numBotPieces = board.getPieces(color).size();
+	    double numOpponentPieces = board.getPieces(PieceColor.opposite(color)).size();
+	    
+	    return numBotPieces / (numBotPieces + numOpponentPieces);
+	}
+	
+	@Override
+    public String getName() {
+        return HEURISTIC_DESCRIPTION;
+    }
+}
