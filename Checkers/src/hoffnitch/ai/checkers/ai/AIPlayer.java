8e0adb7336516ff5694c7dd481224f667ca47b3e
Mike <mikenitchie@gmail.com>
2014-10-08 13:43:34 -0400

    Stubbed out several heuristic functions in AIPlayer

--- Checkers/src/hoffnitch/ai/checkers/ai/AIPlayer.java
+++ Checkers/src/hoffnitch/ai/checkers/ai/AIPlayer.java
@@ -20,7 +20,7 @@ public abstract class AIPlayer extends Player {
 	public AIPlayer(String name, PieceColor color) {
         super(name, color);
         ratioWeight = 1;
-        kingWeight = 1;
+        kingWeight = .4;
         distanceWeight = 1;
     }
     
@@ -85,6 +85,11 @@ public abstract class AIPlayer extends Player {
 		return turnTree.getBestTurn();
 	}
 	
+	/**
+	 * Scores the board based on the ratio of player pieces to opponent pieces
+	 * @param board The current game board
+	 * @return A double representing the ratio of playerPieces:totalPieces
+	 */
 	protected double scoreBoardOnPieceRatio(GameState board) {
 	    double numBotPieces = board.getPieces(color).size();
         double numOpponentPieces = board.getPieces(PieceColor.opposite(color)).size();
@@ -92,6 +97,11 @@ public abstract class AIPlayer extends Player {
         return numBotPieces / (numBotPieces + numOpponentPieces);
 	}
 	
+	/**
+     * Scores the board based on the ratio of player kings to opponent kings
+     * @param board The current game board
+     * @return A double representing the ratio of playerKings:totalKings
+     */
 	protected double scoreBoardOnNumKings(GameState board) {
 	    List<Piece> botPieces = board.getPieces(color);
 	    List<Piece> opponentPieces = board.getPieces(PieceColor.opposite(color));
@@ -117,7 +127,87 @@ public abstract class AIPlayer extends Player {
 	    return toReturn;
 	}
 	
+	/**
+	 * Scores the board based on the aggregate distance of all player pieces
+	 * to all opponent pieces. The purpose is to 'nudge' the piece nearer
+	 * to the opponent to avoid a stupid stalemate of pieces moving back and forth.
+	 * @param board
+	 * @return
+	 */
 	protected double scoreBoardOnDistanceToOpponent(GameState board) {
 	    return 0;
 	}
+	
+	/**
+	 * Returns an int representing the number of your pieces remaining in
+	 * the back row, preventing the opponent from being kinged.
+	 * @param board
+	 * @return The number of pieces in the back row.
+	 */
+	protected int scoreBoardOnPromotionPrevention(GameState board) {
+	    int numProtectors = 0;
+	    int min = 0;
+	    int max = 0;
+	    switch (color) {
+	    case DARK:
+	        min = 1;
+	        max = 4;
+	        break;
+	    case LIGHT:
+	        min = 29;
+	        max = 32;
+	        break;
+	    }
+	    
+	    for (int i = min; i <= max; i++) {
+            if (board.getPieceAtPosition(i).color.equals(color)) {
+                numProtectors++;
+            }
+        }
+	    return numProtectors;
+	}
+	
+	/**
+	 * Returns a value representing the aggregate distance between the
+	 * pawns and the back row. Closer distance is desired, but the function
+	 * will return a "High" number if the distance is small, and a "low" number
+	 * if the distance is large.
+	 * @param board
+	 * @return
+	 */
+	protected int scoreBoardOnDistanceToPromotion(GameState board) {
+	    // The distance should be inversed. That is, if a piece is
+	    // 10 spaces away, the value should be 1/10. 
+	    return 0;
+	}
+	
+	/**
+	 * Scores the board based on the open spaces on the opponent's back row
+	 * @param board
+	 * @return
+	 */
+	protected int scoreBoardOnOpenPromotionSpaces(GameState board) {
+	    return 0;
+	}
+	
+	/**
+	 * Scores the board based on the number of pieces that can make a move
+	 * (aren't stuck)
+	 * @param board
+	 * @return
+	 */
+	protected int scoreBoardOnNumMoveablePieces(GameState board) {
+	    return 0;
+	}
+	
+	/**
+	 * Scores the board based on the number of pieces on an.
+	 * @param board
+	 * @return
+	 */
+	protected int scoreBoardOnNumSafePieces(GameState board) {
+	    return 0;
+	}
+	
+	
 }
